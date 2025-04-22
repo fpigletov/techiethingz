5ce52c6959a872d2bfff0b5ad6926e6eaff3b134
Harnish <harrnish@gmail.com>
2025-04-22 12:05:11 -0400

    Menu overlay animation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Menu from "@/components/Menu/Menu";
 
 export const metadata = {
   title: "Format Archive",
@@ -19,7 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <Menu />
         {children}
       </body>
     </html>
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,177 @@
+"use client";
+import "./Menu.css";
+
+import { useRef } from "react";
+import Link from "next/link";
+
+import gsap from "gsap";
+import CustomEase from "gsap/CustomEase";
+import { useGSAP } from "@gsap/react";
+
+const Menu = () => {
+  const menuRef = useRef(null);
+  const navRef = useRef(null);
+  const menuOverlayRef = useRef(null);
+
+  const navLogoRef = useRef(null);
+  const menuBtnRef = useRef(null);
+  const cartBtnRef = useRef(null);
+
+  const overlayLogoRef = useRef(null);
+  const closeBtnRef = useRef(null);
+
+  gsap.registerPlugin(CustomEase);
+  CustomEase.create("hop", ".15, 1, .25, 1");
+
+  useGSAP(
+    () => {
+      gsap.set(menuOverlayRef.current, {
+        clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+        pointerEvents: "none",
+      });
+
+      gsap.set([overlayLogoRef.current, closeBtnRef.current], {
+        y: "100%",
+      });
+    },
+    { scope: menuRef }
+  );
+
+  const openMenu = () => {
+    const tl = gsap.timeline();
+
+    tl.to([navLogoRef.current, menuBtnRef.current, cartBtnRef.current], {
+      y: "-100%",
+      duration: 0.5,
+      stagger: 0.1,
+      ease: "power3.out",
+      onComplete: () => {
+        navRef.current.style.pointerEvents = "none";
+
+        gsap.set([navLogoRef.current, menuBtnRef.current, cartBtnRef.current], {
+          y: "100%",
+        });
+      },
+    });
+
+    tl.to(
+      menuOverlayRef.current,
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 1,
+        ease: "hop",
+        onStart: () => {
+          menuOverlayRef.current.style.pointerEvents = "all";
+        },
+      },
+      "-=0.5"
+    );
+
+    tl.to(
+      [overlayLogoRef.current, closeBtnRef.current],
+      {
+        y: "0%",
+        duration: 0.5,
+        stagger: 0.1,
+        ease: "power3.out",
+      },
+      "-=0.35"
+    );
+  };
+
+  const closeMenu = () => {
+    const tl = gsap.timeline();
+
+    tl.to([overlayLogoRef.current, closeBtnRef.current], {
+      y: "-100%",
+      duration: 0.5,
+      stagger: 0.1,
+      ease: "power3.out",
+    });
+
+    tl.to(
+      menuOverlayRef.current,
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        duration: 1,
+        ease: "hop",
+        onComplete: () => {
+          menuOverlayRef.current.style.pointerEvents = "none";
+
+          gsap.set(menuOverlayRef.current, {
+            clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+          });
+
+          gsap.set([overlayLogoRef.current, closeBtnRef.current], {
+            y: "100%",
+          });
+        },
+      },
+      "-=0.5"
+    );
+
+    tl.to(
+      [navLogoRef.current, menuBtnRef.current, cartBtnRef.current],
+      {
+        y: "0%",
+        duration: 0.5,
+        stagger: 0.1,
+        ease: "power3.out",
+        onStart: () => {
+          navRef.current.style.pointerEvents = "all";
+        },
+      },
+      "-=0.35"
+    );
+  };
+
+  return (
+    <div className="menu" ref={menuRef}>
+      <div className="nav" ref={navRef}>
+        <div className="nav-logo">
+          <div className="revealer">
+            <Link href="/" ref={navLogoRef}>
+              Format Archive
+            </Link>
+          </div>
+        </div>
+
+        <div className="nav-items">
+          <div className="nav-menu-toggle-open">
+            <div className="revealer" onClick={openMenu}>
+              <p ref={menuBtnRef}>Menu</p>
+            </div>
+          </div>
+
+          <div className="nav-cart-btn">
+            <div className="revealer">
+              <p ref={cartBtnRef}>
+                Cart (<span id="cart-count">0</span>)
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="menu-overlay" ref={menuOverlayRef}>
+        <div className="menu-overlay-nav">
+          <div className="menu-overlay-nav-logo">
+            <div className="revealer">
+              <Link href="/" ref={overlayLogoRef}>
+                Format Archive
+              </Link>
+            </div>
+          </div>
+
+          <div className="menu-overlay-nav-toggle-close">
+            <div className="revealer" onClick={closeMenu}>
+              <p ref={closeBtnRef}>Close</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
