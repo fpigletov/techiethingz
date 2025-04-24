0dcff35bfd1559d87d5c8f792abaa06a779cb803
Harnish <harrnish@gmail.com>
2025-04-23 22:38:25 -0400

    Removed debug nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <>
-      <div className="debug-nav">
-        <Link href="/catalogue">Catalogue</Link>
-        <Link href="/info">Info</Link>
-      </div>
-    </>
-  );
+  return <></>;
 }
