19e62d88bf9e32d53133b7b7695e5b8e7e8c3072
Harnish <harrnish@gmail.com>
2025-04-25 00:30:35 -0400

    Article page animations

--- src/app/editorial/[slug]/page.jsx
+++ src/app/editorial/[slug]/page.jsx
@@ -5,15 +5,23 @@ import articles from "@/articles";
 import { findArticleBySlug, generateSlug } from "@/utils";
 import { useParams } from "next/navigation";
 import { useTransitionRouter } from "next-view-transitions";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Footer from "@/components/Footer/Footer";
-import { ReactLenis } from "lenis/react";
+import { ReactLenis, useLenis } from "lenis/react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import SplitType from "split-type";
 
 const ArticleDetail = () => {
   const { slug } = useParams();
   const article = findArticleBySlug(articles, slug);
   const [isAnimating, setIsAnimating] = useState(false);
   const router = useTransitionRouter();
+
+  const containerRef = useRef(null);
+  const descriptionRefs = useRef([]);
+
+  const lenis = useLenis(({ scroll }) => {});
 
   function slideInOut() {
     document.documentElement.animate(
@@ -67,6 +75,71 @@ const ArticleDetail = () => {
     }, 1500);
   };
 
+  useGSAP(() => {
+    if (!containerRef.current || !article) return;
+
+    gsap.set(".article-meta .revealer p", {
+      y: "100%",
+    });
+
+    const tl = gsap.timeline({
+      defaults: {
+        ease: "power3.out",
+        delay: 0.85,
+      },
+    });
+
+    tl.to(".article-meta .revealer p", {
+      y: "0%",
+      duration: 0.75,
+      stagger: 0.05,
+    });
+
+    tl.fromTo(
+      ".article-banner-img",
+      {
+        y: 300,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power3.out",
+      },
+      "-=2"
+    );
+
+    descriptionRefs.current.forEach((ref, index) => {
+      if (ref) {
+        const splitDescription = new SplitType(ref, {
+          types: "lines",
+          lineClass: "line",
+        });
+
+        splitDescription.lines.forEach((line) => {
+          const content = line.innerHTML;
+          line.innerHTML = `<span>${content}</span>`;
+        });
+
+        gsap.set(`#article-paragraph-${index} .line span`, {
+          y: "100%",
+          display: "block",
+        });
+
+        tl.to(
+          `#article-paragraph-${index} .line span`,
+          {
+            y: "0%",
+            duration: 0.75,
+            stagger: 0.05,
+          },
+          "-=1.5"
+        );
+      }
+    });
+  }, [article, containerRef]);
+
   if (!article) {
     return (
       <div className="article-not-found">
@@ -97,15 +170,86 @@ const ArticleDetail = () => {
         syncTouch: true,
       }}
     >
-      <div className="article-detail-page">
-        <div className="article-banner">
-          <img
-            src={`/product_images/${article.bannerImg}`}
-            alt={article.title}
-            className="article-banner-img"
-          />
+      <div className="article-detail-page" ref={containerRef}>
+        <div className="article-content">
+          <div className="article-detail-col">
+            <div className="article-banner-img">
+              <img
+                src={`/product_images/${article.bannerImg}`}
+                alt={article.title}
+              />
+            </div>
+
+            <div className="article-copy">
+              <p
+                id="article-paragraph-0"
+                ref={(el) => (descriptionRefs.current[0] = el)}
+              >
+                {article.bodyCopy[0]}
+              </p>
+            </div>
+
+            <div className="article-copy">
+              <p
+                id="article-paragraph-1"
+                ref={(el) => (descriptionRefs.current[1] = el)}
+              >
+                {article.bodyCopy[1]}
+              </p>
+            </div>
+
+            <div className="article-copy">
+              <p
+                id="article-paragraph-2"
+                ref={(el) => (descriptionRefs.current[2] = el)}
+              >
+                {article.bodyCopy[2]}
+              </p>
+            </div>
+          </div>
+          <div className="article-detail-col article-meta">
+            <div className="article-date">
+              <div className="revealer">
+                <p>Date</p>
+              </div>
+              <div className="revealer">
+                <p>{article.date}</p>
+              </div>
+            </div>
+            <div className="article-title">
+              <div className="revealer">
+                <p>Article Name</p>
+              </div>
+              <div className="revealer">
+                <p>{article.title}</p>
+              </div>
+            </div>
+
+            <div className="article-author">
+              <div className="revealer">
+                <p>Author</p>
+              </div>
+
+              <div className="revealer">
+                <p>By {article.author}</p>
+              </div>
+            </div>
+
+            <div className="article-tags">
+              <div className="revealer">
+                <p>Tags</p>
+              </div>
+
+              <div className="tags">
+                {article.tags.map((tag, index) => (
+                  <div className="revealer" key={index}>
+                    <p>{tag}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-        <p className="article-title">{article.title}</p>
         <Footer />
       </div>
     </ReactLenis>
